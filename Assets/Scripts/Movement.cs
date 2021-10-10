9b0f1376d948946a5c8b4a61dc0444e805a960f6
AnitaLim123 <anita.lim@ontariotechu.net>
2021-10-09 20:08:44 -0400

    Added Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movespeed;
+    private float dirX, dirZ;
+    //public bool onGround = true;
+
+    void Start()
+    {
+        movespeed = 3f;
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * movespeed;
+        dirZ = Input.GetAxis("Vertical") * movespeed;
+        
+        rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            //onGround = false;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+    }
+
+
+}
